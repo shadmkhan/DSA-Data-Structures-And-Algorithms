1aca76a5621d9e485ba7ccd0ba9b9f45e4eaf4ee
Shad M Khan <sm.khan2010@gmail.com>
2017-04-24 02:30:25 +0530

    Update boundaries.java

--- DSA/src/boundaries.java
+++ DSA/src/boundaries.java
@@ -1,7 +1,5 @@
-
-
 // Print Boundaries of a binary tree
-//Author: Shad M Khan
+// Author: Shad M Khan
 
 class QNode
 	{
@@ -16,53 +14,53 @@ class QNode
 			
 		}
 	}
-	public void printBoundaries(Node root)
-	{
-		if(root==null) return;
-		Queue<QNode> qN = new LinkedList<>();
-		qN.add(new QNode(root, 0)); //Adding the root
-		Node prev=null;
-		
-		//Hashmap to store Level and Count at each level;
-		HashMap<Integer, Integer> mapLevels = new HashMap<>();
-		
-		while(!qN.isEmpty())
-		{
-			QNode temp=qN.remove();
-			
-			//Print leaf nodes
-			if(temp.n.left==null && temp.n.right==null)
-			{
-				System.out.println(temp.n.data);
-			}
-			if(mapLevels.containsKey(temp.level))
-			{
-				prev=temp.n;			
-				mapLevels.put(temp.level, mapLevels.get(temp.level)+1);
-			}
-			else
-			{
-				mapLevels.put(temp.level,0);
+public void printBoundaries(Node root)
+{
+	if(root==null) return;
+	Queue<QNode> qN = new LinkedList<>();
+	qN.add(new QNode(root, 0)); //Adding the root
+	Node prev=null;
 
-				
-				//We already printed leaf nodes so avoid re-printing them
-				if(!(temp.n.left==null && temp.n.right==null))
-				{
-					//Print last node of the last level
-					if(prev!=null) 
-					{
-						System.out.println(prev.data);
-					}
-					//Print first node of this level
-						System.out.println(temp.n.data);
-				}
-				
-			}
-			
-			if(temp.n.left!=null)
-				qN.add(new QNode(temp.n.left,temp.level+1));
-			if(temp.n.right!=null)
-				qN.add(new QNode(temp.n.right,temp.level+1));
-			
+	//Hashmap to store Level and Count at each level;
+	HashMap<Integer, Integer> mapLevels = new HashMap<>();
+
+	while(!qN.isEmpty())
+	{
+		QNode temp=qN.remove();
+
+		//Print leaf nodes
+		if(temp.n.left==null && temp.n.right==null)
+		{
+			System.out.println(temp.n.data);
 		}
+		if(mapLevels.containsKey(temp.level))
+		{
+			prev=temp.n;			
+			mapLevels.put(temp.level, mapLevels.get(temp.level)+1);
+		}
+		else
+		{
+			mapLevels.put(temp.level,0);
+
+
+			//We already printed leaf nodes so avoid re-printing them
+			if(!(temp.n.left==null && temp.n.right==null))
+			{
+				//Print last node of the last level
+				if(prev!=null) 
+				{
+					System.out.println(prev.data);
+				}
+				//Print first node of this level
+					System.out.println(temp.n.data);
+			}
+
+		}
+
+		if(temp.n.left!=null)
+			qN.add(new QNode(temp.n.left,temp.level+1));
+		if(temp.n.right!=null)
+			qN.add(new QNode(temp.n.right,temp.level+1));
+
 	}
+}
